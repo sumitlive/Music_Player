26182f14f76c5cf09913265ad5125393dd2f7d92
sumitlive <119405145+sumitlive@users.noreply.github.com>
2025-01-13 16:24:28 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
+import React from "react";
+import Player from "./components/player";
+import "./app.css";
 
+function App() {
+  return (
+    <div className="App">
+      <Player />
+    </div>
+  );
+}
+
+export default App;
